0c6595ae9dad06dbcc343aefae4bdbaf59b0c7aa
Maks <alisa3d2007@gmail.com>
2025-02-17 13:11:50 +0300

    feat: updated style in swiper on the home page

--- theme/template-parts/blocks/product-card-horizontal.php
+++ theme/template-parts/blocks/product-card-horizontal.php
@@ -5,10 +5,10 @@ $in_cart = false;
 
 // Проверяем, находится ли товар в корзине
 foreach (WC()->cart->get_cart() as $cart_item_key => $cart_item) {
-	if ($cart_item['product_id'] === $product->get_id()) {
-		$in_cart = true;
-		break;
-	}
+    if ($cart_item['product_id'] === $product->get_id()) {
+        $in_cart = true;
+        break;
+    }
 }
 ?>
 <div class="items-start product-card relative bg-white shadow flex flex-col 2xl:flex-row 2xl:rounded-[10px] border border-[#EEEEEE]">
@@ -31,18 +31,18 @@ foreach (WC()->cart->get_cart() as $cart_item_key => $cart_item) {
 			</div>
 
 			<?php
-			$author = $product->get_attribute('book-author');
+            $author = $product->get_attribute('book-author');
 
-			if (!empty($author)) {
-				echo '<div class="text-primary 2xl:text-xl">' . $author . '</div>';
-			}
-			?>
+if (!empty($author)) {
+    echo '<div class="text-primary 2xl:text-xl">' . $author . '</div>';
+}
+?>
 
 		</div>
 
 		<?php
-		$content = get_the_content();
-		if ($content) : ?>
+        $content = get_the_content();
+if ($content) : ?>
 			<div class="text-sm leading-7 text-primary 2xl:pl-5 2xl:border-l 2xl:border-[#C5C5C5] mt-3 mb-3">
 				<div class="hidden 2xl:block"><?php echo wp_trim_words(get_the_content(), 50, '...'); ?></div>
 				<div class="2xl:hidden"><?php echo wp_trim_words(get_the_content(), 34, '...'); ?></div>
@@ -60,12 +60,12 @@ foreach (WC()->cart->get_cart() as $cart_item_key => $cart_item) {
 		<div class="flex gap-6 w-full items-center justify-start ">
 			<div class="relative w-3/5 2xl:w-1/2">
 				<?php
-				if ($in_cart) {
-					echo '<a href="' . esc_url($cart_url) . '" class="btn bg-primary text-center hover:ring-primary hover:ring-opacity-30" title="Просмотр корзины"><span>Уже в корзине</span></a>';
-				} else {
-					echo '<a href="' . esc_url($product->add_to_cart_url()) . '" data-quantity="1" class="btn button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="' . $product->get_id() . '" data-product_sku="' . esc_attr($product->get_sku()) . '" aria-label="' . esc_attr($product->add_to_cart_description()) . '" rel="nofollow">В корзину</a>';
-				}
-				?>
+        if ($in_cart) {
+            echo '<a href="' . esc_url($cart_url) . '" class="btn bg-primary text-center hover:ring-primary hover:ring-opacity-30" title="Просмотр корзины"><span>Уже в корзине</span></a>';
+        } else {
+            echo '<a href="' . esc_url($product->add_to_cart_url()) . '" data-quantity="1" class="btn button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="' . $product->get_id() . '" data-product_sku="' . esc_attr($product->get_sku()) . '" aria-label="' . esc_attr($product->add_to_cart_description()) . '" rel="nofollow">В корзину</a>';
+        }
+?>
 			</div>
 			<a href="<?php the_permalink(); ?>" class="btn-border h-[50px] font-semibold w-2/5 2xl:w-1/2"><span>Подробнее</span></a>
 
--- theme/template-parts/blocks/product-card.php
+++ theme/template-parts/blocks/product-card.php
@@ -5,16 +5,16 @@ $in_cart = false;
 
 // Проверяем, находится ли товар в корзине
 foreach (WC()->cart->get_cart() as $cart_item_key => $cart_item) {
-  if ($cart_item['product_id'] === $product->get_id()) {
-    $in_cart = true;
-    break;
-  }
+    if ($cart_item['product_id'] === $product->get_id()) {
+        $in_cart = true;
+        break;
+    }
 }
 
 $ucenennyj_value = $product->get_attribute('pa_ucenennyj');
 $ucenenn = '';
 if ('да' === strtolower($ucenennyj_value)) {
-  $ucenenn = "уцененный";
+    $ucenenn = "уцененный";
 }
 ?>
 <?php global $product; ?>
@@ -29,13 +29,13 @@ if ('да' === strtolower($ucenennyj_value)) {
 
   <?php
   $availability = '';
-  if ($product->get_stock_quantity() > 1 && $product->get_stock_quantity() < 6) {
-  ?>
+if ($product->get_stock_quantity() > 1 && $product->get_stock_quantity() < 6) {
+    ?>
     <div class="absolute flex justify-center top-32 z-20 -right-[68px] max-xl:rotate-90 xl:top-0 xl:right-0 w-40 xl:w-full">
       <div class="bg-primary text-white flex items-center h-6 xl:h-9 px-8 font-title text-sm xl:text-xl rounded-b-lg z-10">
         <?php
-        $availability = __('осталось  ', 'woocommerce') . $product->get_stock_quantity() . ' шт.';
-        echo $availability; ?>
+          $availability = __('осталось  ', 'woocommerce') . $product->get_stock_quantity() . ' шт.';
+    echo $availability; ?>
       </div>
     </div>
   <?php  } ?>
@@ -47,7 +47,7 @@ if ('да' === strtolower($ucenennyj_value)) {
     <?php echo do_shortcode('[yith_wcwl_add_to_wishlist]'); ?>
   </div>
 
-  <a href="<?php the_permalink(); ?>" class="mt-10 group border-b-2 border-[#F6F6F6] h-[280px] 2xl:h-[400px] flex items-center justify-center overflow-hidden">
+  <a href="<?php the_permalink(); ?>" class="group border-b-2 border-[#F6F6F6] h-[320px] 2xl:h-[440px] flex items-center justify-center overflow-hidden">
     <img src="<?php echo get_the_post_thumbnail_url(); ?>" class="group-hover:scale-105 transition-all" alt="<?php the_title(); ?>">
   </a>
 
@@ -65,12 +65,12 @@ if ('да' === strtolower($ucenennyj_value)) {
 
         <div class="relative w-3/5 2xl:w-1/2">
           <?php
-          if ($in_cart) {
-            echo '<a href="' . esc_url($cart_url) . '" class="btn text-center bg-primary hover:ring-primary hover:ring-opacity-30" title="Просмотр корзины"><span>Уже в корзине</span></a>';
-          } else {
-            echo '<a href="' . esc_url($product->add_to_cart_url()) . '" data-quantity="1" class="btn button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="' . $product->get_id() . '" data-product_sku="' . esc_attr($product->get_sku()) . '" aria-label="' . esc_attr($product->add_to_cart_description()) . '" rel="nofollow">В корзину</a>';
-          }
-          ?>
+      if ($in_cart) {
+          echo '<a href="' . esc_url($cart_url) . '" class="btn text-center bg-primary hover:ring-primary hover:ring-opacity-30" title="Просмотр корзины"><span>Уже в корзине</span></a>';
+      } else {
+          echo '<a href="' . esc_url($product->add_to_cart_url()) . '" data-quantity="1" class="btn button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="' . $product->get_id() . '" data-product_sku="' . esc_attr($product->get_sku()) . '" aria-label="' . esc_attr($product->add_to_cart_description()) . '" rel="nofollow">В корзину</a>';
+      }
+?>
         </div>
 
         <a href="<?php the_permalink(); ?>" class="btn-border h-[50px] w-2/5 2xl:w-1/2"><span>Подробнее</span></a>
